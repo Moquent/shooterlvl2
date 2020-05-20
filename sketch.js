49ed6b2f91c35783b71fa6e6a5c1884cdfdc7133
TejeshCodes <tejeshreddy907@gmail.com>
2020-05-20 14:28:18 +0530

    commit

--- sketch.js
+++ sketch.js
@@ -1,9 +1,19 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var ground;
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  var canvas = createCanvas(1200,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(600,height,1200,20);
 }
 
 function draw() {
   background(255,255,255);  
+  Engine.update(engine);
+
   drawSprites();
 }
