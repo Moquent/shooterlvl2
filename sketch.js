2ab52af7a2becfeca132ebbe143de3c33df86199
TejeshCodes <tejeshreddy907@gmail.com>
2020-05-22 12:17:20 +0530

    commit

--- sketch.js
+++ sketch.js
@@ -2,12 +2,42 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 
-var ax = 900, ay = 30;
+var box1, box2, box3, box4, box5, box6, box7, box8, box9, box0;
 var ground;
 function setup() {
   var canvas = createCanvas(1200,400);
   engine = Engine.create();
   world = engine.world;
+
+  box1 = new Box(900, 370);
+  World.add(world, box1);
+
+  box2 = new Box(919, 370);
+  World.add(world, box2);
+
+  box3 = new Box(938, 370);
+  World.add(world, box3);
+
+  box4 = new Box(957, 370);
+  World.add(world, box4);
+
+  box5 = new Box(909, 340);
+  World.add(world, box5);
+  
+  box6 = new Box(928, 340);
+  World.add(world, box6);
+
+  box7 = new Box(947, 340);
+  World.add(world, box7);
+
+  box8 = new Box(918, 310);
+  World.add(world, box8);
+
+  box9 = new Box(937, 310);
+  World.add(world, box9);
+
+  box0 = new Box(927, 280);
+  World.add(world, box0);
 
   ground = new Ground(600,height,1200,20);
   World.add(world, ground);
@@ -17,22 +47,18 @@ function draw() {
   background("yellow");  
   Engine.update(engine);
 
-  for(var i = 1; i <= 6 ; i++){
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  box6.display();
+  box7.display();
+  box8.display();
+  box9.display();
+  box0.display();
 
-    ax = ax + 18;
-    if(i == 3){
-      ax = 900 + (36/2);
-      ay = ay + 42;
-    }
-
-    if(i == 5){
-      ax = 900 + (27/2);
-      ay = ay + 42;
-    }
-
-    fill("red");
-    var box = new Box(ax, ay);
-  }
+  ground.display();
 
   drawSprites();
 }
