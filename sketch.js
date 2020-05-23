6c0563d2610b1fb15005b1ba6e1ffd0b223de60b
TejeshCodes <63721912+TejeshCodes@users.noreply.github.com>
2020-05-23 19:13:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,77 +1,35 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+class Sling{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+    
+    fly(){
+        this.sling.bodyA = null;
+    }
 
-var box1, box2, box3, box4, box5, box6, box7, box8, box9, box0;
-var sling, holder, ball;
-var ground;
-function setup() {
-  var canvas = createCanvas(1200,400);
-  engine = Engine.create();
-  world = engine.world;
-
-  box1 = new Box(900, 370);
-  World.add(world, box1);
-
-  box2 = new Box(919, 370);
-  World.add(world, box2);
-
-  box3 = new Box(938, 370);
-  World.add(world, box3);
-
-  box4 = new Box(957, 370);
-  World.add(world, box4);
-
-  box5 = new Box(909, 340);
-  World.add(world, box5);
-  
-  box6 = new Box(928, 340);
-  World.add(world, box6);
-
-  box7 = new Box(947, 340);
-  World.add(world, box7);
-
-  box8 = new Box(918, 310);
-  World.add(world, box8);
-
-  box9 = new Box(937, 310);
-  World.add(world, box9);
-
-  box0 = new Box(927, 280);
-  World.add(world, box0);
-
-  holder = Bodies.rectangle(1000, 200, 5, 5);
-
-  ball = Bodies.rectangle(1000, 200, 15, 15, {density: 0.004});
-
-  ground = new Ground(600,height,1200,20);
-  World.add(world, ground);
-
-  sling = new Sling(holder.body, {x:1000, y: 200});
-}
-
-function draw() {
-  background("yellow");  
-  Engine.update(engine);
-
-  fill("black");
-  rect(ball.position.x, ball.position.y, ball.width, ball.height);
-
-  box1.display();
-  box2.display();
-  box3.display();
-  box4.display();
-  box5.display();
-  box6.display();
-  box7.display();
-  box8.display();
-  box9.display();
-  box0.display();
-
-  sling.display();
-
-  ground.display();
-
-  drawSprites();
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+        
+            push();
+            
+            stroke(48,22,8);
+            strokeWeight(7);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            
+            pop();
+        }
+    }
 }
