50702284ddebf8a62e93d30aedceb5be74169674
TejeshCodes <63721912+TejeshCodes@users.noreply.github.com>
2020-05-26 19:59:20 +0530

    Add files via upload

--- ball.js
+++ ball.js
@@ -3,12 +3,12 @@ class Ball{
         var options = {
             'restitution':0.8,
             'friction':1.0,
-            'density':1.0
+            'density':3.0
         }
     
-        this.body = Bodies.rectangle(200, 200, 5, 5, options);
-        this.width = 5;
-        this.height = 5;
+        this.body = Bodies.rectangle(200, 200, 40, 40, options);
+        this.width = 40;
+        this.height = 40;
 
         World.add(world, this.body);
     }
@@ -16,6 +16,6 @@ class Ball{
     display(){
         fill("black");
         rectMode(CENTER);
-        rect(this.body.position.x, this.body.position.y, this.body.width, this.body.height);
+        rect(this.body.position.x, this.body.position.y, this.width, this.height);
     }
 }
